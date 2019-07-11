7fd25e14a59a1fff4057c218e86d509214036bb0
Val Neekman <val@neekware.com>
2019-07-11 08:33:47 -0400

    define base class for unsubService

--- libs/ng/ag/unsub/src/index.ts
+++ libs/ng/ag/unsub/src/index.ts
@@ -1,4 +1,5 @@
 export { UnsubscribableOptions } from './lib/unsub.models';
 export { DefaultUnsubscribableOptions } from './lib/unsub.defaults';
+export { UnsubManager } from './lib/unsub.manager';
 export { UnsubService } from './lib/unsub.service';
 export { Unsubscribable } from './lib/unsub.decorators';
--- libs/ng/ag/unsub/src/lib/unsub.service.ts
+++ libs/ng/ag/unsub/src/lib/unsub.service.ts
@@ -1,35 +1,18 @@
 import { Injectable, OnDestroy } from '@angular/core';
 
-import { Subject, Subscription } from 'rxjs';
-import { isFunction } from '@agx/utils';
+import { Subject } from 'rxjs';
+
+import { UnsubManager } from './unsub.manager';
 
 /** An injectable service class that handles auto cancellation of subscriptions */
 @Injectable()
-export class UnsubService implements OnDestroy {
+export class UnsubService extends UnsubManager implements OnDestroy {
   destroy$: Subject<boolean> = new Subject<boolean>();
-  subscriptions: Subscription[] = [];
-
-  /**
-   * Registers all subscriptions that need auto cancellation on destroy
-   * @param subscription Subscription or a list of Subscription
-   */
-  autoUnsubscribe(subscription: Subscription | Subscription[]) {
-    if (Array.isArray(subscription)) {
-      this.subscriptions = [...this.subscriptions, ...subscription];
-    } else {
-      this.subscriptions.push(subscription);
-    }
-  }
 
   /** Handles auto cancellation of all subscriptions on destroy */
   ngOnDestroy() {
     this.destroy$.next(true);
     this.destroy$.complete();
-
-    this.subscriptions.forEach(subscription => {
-      if (subscription && subscription.unsubscribe && isFunction(subscription.unsubscribe)) {
-        subscription.unsubscribe();
-      }
-    });
+    this.unsubscribe();
   }
 }
